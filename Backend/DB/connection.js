175538f974ebe741cb41db3889cbe17d3e4c75ec
Athul-M1 <athul10881@gmail.com>
2024-12-30 16:55:13 +0530

    Delete Backend directory

--- Backend/DB/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const connectionString = process.env.DATABASE
-
-mongoose.connect(connectionString).then(()=>{
-    console.log("Mongodb connection sucessful")
-}).catch((err)=>{
-    console.log("connection failed",err)
-})
